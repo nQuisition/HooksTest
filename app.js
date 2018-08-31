db3d3c9e13bd82505fcb2551672a801545d3d43e
Ivan <nquisition@gmail.com>
2018-09-01 05:21:18 +0900

    Commit

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
 const app = express();
+const port = process.env.PORT || 8010;
+
 app.set("trust proxy", "127.0.0.1");
 
 app.get("/", (req, res) => {
   res.status(200).send("Hooks test!!!");
 });
 
-app.listen(8010, () => {
-  console.log("Hooks app listening on port 8010!");
+app.listen(port, () => {
+  console.log(`Hooks app listening on port ${port}!`);
 });
